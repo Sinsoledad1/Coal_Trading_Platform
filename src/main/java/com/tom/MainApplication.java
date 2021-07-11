c00422dd3aa571480e9cf91966dfab2729153ced
北辰 <59138540+Sinsoledad1@users.noreply.github.com>
2021-07-12 00:41:08 +0800

    总体架构文件添加

---
+++ src/main/java/com/tom/MainApplication.java
@@ -0,0 +1,24 @@
+package com.tom;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author BeiChen
+ * @version 1.0
+ * @date 2020/11/28 12:24
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
