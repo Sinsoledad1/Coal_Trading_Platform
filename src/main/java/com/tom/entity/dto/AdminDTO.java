c7152842ecd852e20dc36a3644bd213370029e79
北辰 <59138540+Sinsoledad1@users.noreply.github.com>
2021-08-12 21:56:52 +0800

    小修改

--- src/main/java/com/tom/entity/dto/AdminDTO.java
+++ src/main/java/com/tom/entity/dto/AdminDTO.java
@@ -18,9 +18,6 @@ public class AdminDTO {
     /**
      * 真实姓名
      * 部门
-     * 身份(0:普通管理员,1:超级管理员)
-     * 账号状态(0:待审核,1:拒绝,2:通过)
-     * 添加时间
      */
     @NotBlank(message = "真实姓名不能为空！")
     private String name;
